1472498d48e8fab7e025a4e974df9543c7cbed64
keshavshetty00 <120775552+keshavshetty00@users.noreply.github.com>
2025-12-10 09:38:14 +0530

    Reformat ItemDto class for improved readability

--- demo/src/main/java/com/rfp/dto/ItemDto.java
+++ demo/src/main/java/com/rfp/dto/ItemDto.java
@@ -5,12 +5,23 @@ public class ItemDto {
     private String name;
     private String specs;
 
-    public int getQty() { return qty; }
-    public void setQty(int qty) { this.qty = qty; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getSpecs() { return specs; }
-    public void setSpecs(String specs) { this.specs = specs; }
+    public int getQty(){
+        return qty;
+    }
+    public void setQty(int qty){
+        this.qty = qty;
+    }
+    public String getName(){
+        return name; 
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getSpecs(){
+        return specs; 
+    }
+    public void setSpecs(String specs){
+        this.specs = specs;
+    }
 }
+
